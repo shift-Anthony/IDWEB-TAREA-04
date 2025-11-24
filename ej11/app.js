3ac1441d3d3577826903540b96a99c5a6d13918a
shift-Anthony <anthonymoisescondori@gmail.com>
2025-11-23 23:32:46 -0500

    ej11

---
+++ ej11/app.js
@@ -0,0 +1,10 @@
+import { sumar, restar, multiplicar } from './operaciones.js';
+
+console.log("Suma:", sumar(10, 5));
+console.log("Resta:", restar(10, 5));
+console.log("Multiplicación:", multiplicar(10, 5));
+
+/* Importar solo suma
+   import { sumar } from './operaciones.js';
+   console.log(sumar(10, 5));
+*/
---
+++ ej11/operaciones.js
@@ -0,0 +1,3 @@
+export const sumar = (a, b) => a + b;
+export const restar = (a, b) => a - b;
+export const multiplicar = (a, b) => a * b;
